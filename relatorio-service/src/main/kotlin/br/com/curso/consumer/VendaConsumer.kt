1280c972e9c7019e15d6dd3e79e827ab0567c256
Ernane Sabino <esabino@stefanini.com>
2022-08-20 12:41:16 -0300

    Persistindo dados no Mongo DB

--- relatorio-service/src/main/kotlin/br/com/curso/consumer/VendaConsumer.kt
+++ relatorio-service/src/main/kotlin/br/com/curso/consumer/VendaConsumer.kt
@@ -1,6 +1,7 @@
 package br.com.curso.consumer
 
 import br.com.curso.model.Venda
+import br.com.curso.service.VendaService
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.micronaut.configuration.kafka.annotation.KafkaListener
 import io.micronaut.configuration.kafka.annotation.OffsetReset
@@ -8,11 +9,14 @@ import io.micronaut.configuration.kafka.annotation.Topic
 
 @KafkaListener(offsetReset = OffsetReset.EARLIEST)
 class VendaConsumer(
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val vendaService: VendaService
 ) {
 
     @Topic("ms-vendas")
     fun receberVenda(id: String, vendaJSON: String) {
         val venda = objectMapper.readValue(vendaJSON, Venda::class.java)
+        vendaService.create(venda)
+        println(venda)
     }
 }
